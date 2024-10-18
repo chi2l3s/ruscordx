e32f46c106ccbdfb3a06cc265c84bb38e4e1ec6e
thefreeth <amixolplex@gmail.com>
2024-10-18 15:06:17 +0700

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -22,14 +22,6 @@ const nextConfig = {
    }
 };
 
-module.exports = {
-   typescript: {
-     // !! WARN !!
-     // Dangerously allow production builds to successfully complete even if
-     // your project has type errors.
-     // !! WARN !!
-     ignoreBuildErrors: true,
-   },
- }
+
 
 export default nextConfig;
